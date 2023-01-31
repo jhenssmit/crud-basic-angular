944a6cadf858cf9ca68cd4150e5d818756ff870d
jhenssmit <jhenssmit@gmail.com>
2023-01-31 01:20:23 -0500

    angular-crud v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee'; 
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'crud-angular';
+  employeeArray: Employee[]=[
+    {id:1, name:"jhens", country:"peru"},
+    {id:2, name:"manuel", country:"usa"},
+    {id:3, name:"cesar", country:"japan"},
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit(){
+
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee=new Employee();
+  }
+
+  delete(){
+    if(confirm('Are you sure you want to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
+
 }
